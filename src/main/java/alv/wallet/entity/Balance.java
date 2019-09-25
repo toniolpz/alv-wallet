1bd7cf6a52e24b0d20bc0fb9657a2228699189bb
toniolpz <to.lopez@hotmail.com>
2019-09-25 16:16:48 -0500

    Creation of entities and repository files.

---
+++ src/main/java/alv/wallet/entity/Balance.java
@@ -0,0 +1,11 @@
+package alv.wallet.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "balance")
+public class Balance {
+
+}
