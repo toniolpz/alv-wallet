8847a5b010be201507cc6cf7f77027d98bfc0e10
toniolpz <to.lopez@hotmail.com>
2019-09-25 16:24:02 -0500

    Defining Balance entity with composite ID.

--- src/main/java/alv/wallet/entity/Balance.java
+++ src/main/java/alv/wallet/entity/Balance.java
@@ -1,11 +1,33 @@
 package alv.wallet.entity;
 
-import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import alv.wallet.entity.type.BalanceId;
 
 @Entity
 @Table(name = "balance")
 public class Balance {
 
+    @EmbeddedId
+    private BalanceId id;
+
+    private double amount;
+
+    public BalanceId getId() {
+        return id;
+    }
+
+    public void setId(BalanceId id) {
+        this.id = id;
+    }
+
+    public double getAmout() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
