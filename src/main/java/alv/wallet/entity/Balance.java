be04175e07bc237e770c6844f8995f32846de07c
toniolpz <to.lopez@hotmail.com>
2019-09-26 01:10:13 -0500

    Update Balance entity to have a single Column Primary Key

--- src/main/java/alv/wallet/entity/Balance.java
+++ src/main/java/alv/wallet/entity/Balance.java
@@ -1,29 +1,52 @@
 package alv.wallet.entity;
 
-import javax.persistence.EmbeddedId;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
-
-import alv.wallet.entity.type.BalanceId;
 
 @Entity
 @Table(name = "balance")
 public class Balance {
 
-    @EmbeddedId
-    private BalanceId id;
+    @Id
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name="currency")
+    private String currency;
+
+    @Column(name="amount")
     private double amount;
 
-    public BalanceId getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(BalanceId id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public double getAmout() {
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getCurrency(){
+        return currency;
+    }
+
+    public void setCurrency(String currency){
+        this.currency = currency;
+    }
+
+    public double getAmount() {
         return amount;
     }
 
