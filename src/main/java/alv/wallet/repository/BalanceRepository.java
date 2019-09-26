3811bc78de5f1fa6446a480ddb52e52eaff5bee4
toniolpz <to.lopez@hotmail.com>
2019-09-26 01:11:03 -0500

    Add query methods for Balance Repository

--- src/main/java/alv/wallet/repository/BalanceRepository.java
+++ src/main/java/alv/wallet/repository/BalanceRepository.java
@@ -1,12 +1,18 @@
 package alv.wallet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import alv.wallet.entity.Balance;
-import alv.wallet.entity.type.BalanceId;
 
 @Repository
-public interface BalanceRepository extends JpaRepository<Balance, BalanceId>{
+public interface BalanceRepository extends JpaRepository<Balance, Integer> {
+    List<Balance> findByUserId(int userId);
 
+    @Query("select b from Balance b where b.userId = :userId and b.currency = :currency")
+    Balance findByUserIdAndCurrency(@Param("userId") int userId, @Param("currency") String currency);
 }
