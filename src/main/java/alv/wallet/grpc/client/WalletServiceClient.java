009a9e4de3eb361760af9b374521323ef12e0db9
toniolpz <to.lopez@hotmail.com>
2019-09-26 15:01:47 -0500

    Move web service Client to a client folder

---
+++ src/main/java/alv/wallet/grpc/client/WalletServiceClient.java
@@ -0,0 +1,60 @@
+package alv.wallet.grpc.client;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import alv.wallet.BalanceRequest;
+import alv.wallet.BalanceResponse;
+import alv.wallet.CurrencyType;
+import alv.wallet.DepositRequest;
+import alv.wallet.Empty;
+import alv.wallet.WalletServiceGrpc;
+import alv.wallet.WithdrawRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+@Component
+public class WalletServiceClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WalletServiceClient.class);
+
+    private WalletServiceGrpc.WalletServiceBlockingStub walletServiceBlockingStub;
+
+    @PostConstruct
+    private void init() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
+        walletServiceBlockingStub = WalletServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    public Empty deposit(Integer idUser, double amount, String currency) {
+        try {
+            DepositRequest request = DepositRequest.newBuilder().setUserId(idUser).setAmount(amount)
+                    .setCurrency(currency).build();
+            Empty response = walletServiceBlockingStub.deposit(request);
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public Empty withdraw(Integer idUser, double amount, String currency) {
+        try {
+            WithdrawRequest request = WithdrawRequest.newBuilder().setUserId(idUser).setAmount(amount)
+                    .setCurrency(currency).build();
+            Empty response = walletServiceBlockingStub.withdraw(request);
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public BalanceResponse getBalance(Integer idUser) {
+        BalanceRequest request = BalanceRequest.newBuilder().setUserId(idUser).build();
+        BalanceResponse response = walletServiceBlockingStub.balance(request);
+        return response;
+    }
+}
